a7e95f28e85f64917175f368fed67e1ab531b046
raumedi <raul.medina@alumnos.uva.es>
2020-09-03 22:43:59 +0200

    Fixed questions window. Added delete button. Added keywords button.

--- sql_judge/view/new_scene_menu_questions.py
+++ sql_judge/view/new_scene_menu_questions.py
@@ -1,17 +1,52 @@
 from view.qt_view.new_scene_menu_questions_view import Ui_NewSceneMenuQuestions
 from view.questions.question_row import QuestionRow
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtGui import QPalette
 
 
 class New_scene_menu_questions(Ui_NewSceneMenuQuestions, QWidget):
     def __init__(self):
         super().__init__()
-
+        self._rows = []
+        self._base_color = Qt.gray
+        self._dark_color = QPalette().window().color()
         self.setupUi(self)
 
     def add_question(self, question, answer):
-        new_row = QuestionRow(question, answer)
+        new_row = QuestionRow(question, answer, len(self._rows))
+        new_row.row_deletion.connect(self.delete_question)
+        self._rows.append(new_row)
         self.verticalLayout.addWidget(new_row)
+        self._update_background()
+
+    def _update_background(self):
+        dark_background = True
+        for row in self._rows:
+            row.setAutoFillBackground(True)
+            palette = row.palette()
+            if dark_background:
+                palette.setColor(row.backgroundRole(), self._dark_color)
+                row.setPalette(palette)
+            else:
+                palette.setColor(row.backgroundRole(), self._base_color)
+                row.setPalette(palette)
+            dark_background = not dark_background
+
+    @pyqtSlot(int)
+    def delete_question(self, index):
+        deletion = None
+        for idx, row in enumerate(self._rows):
+            if idx > index:
+                row.index -= 1
+            elif idx < index:
+                pass
+            elif idx == index:
+                deletion = idx
+
+        self._rows.pop(deletion)
+        self.verticalLayout.takeAt(deletion)
+        self._update_background()
 
     def get_question_text(self):
         return self.question_input.toPlainText()
--- sql_judge/view/questions/question_row.py
+++ sql_judge/view/questions/question_row.py
@@ -1,12 +1,19 @@
 from view.qt_view.questions.question_row import Ui_QuestionRow
 
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import pyqtSignal
 
 
 class QuestionRow(QWidget, Ui_QuestionRow):
-    def __init__(self, question, answer):
+    row_deletion = pyqtSignal(int)
+
+    def __init__(self, question, answer, index):
         super().__init__()
         self.setupUi(self)
-
+        self.index = int(index)
         self.question_label.setText(question)
         self.answer_label.setText(answer)
+        self.delete_button.clicked.connect(self.delete_click_redirection)
+
+    def delete_click_redirection(self):
+        self.row_deletion.emit(self.index)
