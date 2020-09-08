4ea9979938dbb68156d0c644db7f86e3025a6cc5
raumedi <raul.medina@alumnos.uva.es>
2020-09-08 10:38:35 +0200

    Added data load in edit scene datagen

--- sql_judge/control/edit_scene_controller.py
+++ sql_judge/control/edit_scene_controller.py
@@ -1,7 +1,9 @@
+from view.new_scene.data_generation.table_data_generation_tab import Table_data_generation_tab
 from control.new_scene_controller import New_scene_controller
 from model.scene import Scene
 
 from dbjudge.connection_manager.manager import Manager
+from dbjudge import squema_recollector
 from PyQt5.QtCore import QObject, pyqtSlot
 
 
@@ -21,8 +23,12 @@ class Edit_scene_controller(New_scene_controller, QObject):
         self.view_config.delete_data_button.clicked.connect(self.delete_data)
         self.view_config.delete_scenario_button.clicked.connect(
             self.delete_scenario)
+
         self.view_data_gen.scenario_changed.connect(
             self.update_current_scenario)
+        self.view_data_gen.comboBox.currentTextChanged.connect(
+            self.load_datagen_data)
+
         self.view_questions.add_question_button.clicked.connect(
             self.add_question)
         self.view_questions.keywords_edition_popped_up.connect(
@@ -39,6 +45,7 @@ class Edit_scene_controller(New_scene_controller, QObject):
     @pyqtSlot(str)
     def update_current_scenario(self, scenario):
         self.current_scenario = scenario
+        self.model.name = scenario
         self.view_config.setCurrentScenario(scenario)
         self.view_data_gen.setCurrentScenario(scenario)
         self.view_questions.setCurrentScenario(scenario)
@@ -56,8 +63,24 @@ class Edit_scene_controller(New_scene_controller, QObject):
         pass
 
     def load_datagen_data(self):
-        pass
+        manager = Manager.singleton_instance
+        manager.select_database(self.model.name)
+        conn = manager.selected_db_connection
+
+        if not self.model.context:
+            self.model.context = squema_recollector.create_context(conn)
+
+        self.view_data_gen.tabWidget.clear()
+        custom_types = self.manager.get_fake_types()
+        for _, table in enumerate(self.model.context.tables):
+            new_tab_page = Table_data_generation_tab(
+                table, custom_types)
+            new_tab_page.table_data_modified.connect(self.update_type)
+            self.view_data_gen.tabWidget.addTab(new_tab_page, table.name)
 
     # QUESTIONS
     def load_questions_data(self):
+        pass
+
+    def save_questions(self):
         pass
