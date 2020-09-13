190627a854891fb29d3e4ff6eb2e3c65dfda39ba
raumedi <raul.medina@alumnos.uva.es>
2020-09-13 20:08:54 +0200

    Added basic pyinstaller configuration and path change dependant on execution type

--- sql_judge/control/main_controller.py
+++ sql_judge/control/main_controller.py
@@ -9,12 +9,18 @@ class Main_controller(QObject):
     def __init__(self, view):
         super().__init__()
 
+        if getattr(sys, 'frozen', False):
+            base_path = os.path.dirname(sys.executable)
+        else:
+            base_path = sys.path[0]
+
         self.main_window = view
         config = configparser.ConfigParser()
         dir_cfg = os.path.join(
-            sys.path[0],
+            base_path,
             'config.ini'
         )
+
         config.read(dir_cfg)
         self.connection_manager = Manager(
             user=config['DATABASE']['user'],
